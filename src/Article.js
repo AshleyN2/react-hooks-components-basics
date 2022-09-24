d8730c4ded7c20dfff4e4e852ee137e68002e5a8
Ashley <ashleynjeri7@gmail.com>
2022-09-24 18:24:13 +0300

    Code along commit

--- src/Article.js
+++ src/Article.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+// Component to showcase an opinion
 function Article() {
   return (
     <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
--- src/Comment.js
+++ src/Comment.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+// component to display a single user's component
 function Comment() {
   return <div>Naturally, I agree with this article.</div>;
 }
